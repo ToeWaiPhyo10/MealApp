668c1a7e8f778a9d3966df5e71f5097a086576c7
ToeWai27 <100681046+ToeWai27@users.noreply.github.com>
2023-10-12 13:24:01 +0630

    meal app with river pod provider

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meal_flutter_app/providers/favourite_provider.dart';
+import 'package:meal_flutter_app/providers/filter_provider.dart';
+import 'package:meal_flutter_app/screens/category.dart';
+import 'package:meal_flutter_app/screens/filters.dart';
+import 'package:meal_flutter_app/screens/meals.dart';
+import 'package:meal_flutter_app/widgets/drawer.dart';
+
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegan: false,
+  Filter.vegetarian: false,
+};
+
+class TabScreen extends ConsumerStatefulWidget {
+  const TabScreen({super.key});
+
+  @override
+  ConsumerState<TabScreen> createState() => _TabScreenState();
+}
+
+class _TabScreenState extends ConsumerState<TabScreen> {
+  int _selectedPageIndex = 0;
+
+  void _setPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  void _setScreen(String identifier) async {
+    Navigator.of(context).pop();
+    if (identifier == 'filters') {
+      await Navigator.of(context).push<Map<Filter, bool>>(
+        MaterialPageRoute(
+          builder: (ctx) => const FiltersScreen(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final availableMeals = ref.watch(filterMealsProvider);
+
+    Widget activePage = CategoryScreen(
+      availableMeal: availableMeals,
+    );
+    var activePageTitle = 'Categories';
+    if (_selectedPageIndex == 1) {
+      final favouriteMeals = ref.watch(favouriteMealsProvider);
+      activePage = MealsScreen(
+        meals: favouriteMeals,
+      );
+      activePageTitle = "Favourites";
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _setPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favourites',
+          ),
+        ],
+      ),
+    );
+  }
+}
